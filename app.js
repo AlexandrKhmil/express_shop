0323a26073ebbe707284172ee3aaec3f58d62d38
Alex <oleksandr.khmil2@gmail.com>
2020-03-29 14:20:30 +0300

    created connection to MYSQL

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+// MYSQL
+const mysql = require('mysql')
+const config = require('./config/default.json')
+const connection = mysql.createConnection(config)
+
 app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-app.get('/q', (req, res) => res.send('q1wq'))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
+app.get('/q', async (req, res) => {
+	connection.query('SELECT* FROM new_table', (err, rows, fields) => {
+		res.send(rows)
+	})
 })
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const start = async () => {
+	try { 
+		app.listen(port, () => console.log(`App's port: ${port}!`))  
+	} catch (e) {
+		console.log('Server Error', e.message)
+		process.exit(1)
+	}
+}
+
+start()
