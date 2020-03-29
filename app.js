a0fa6cb4c00f52b8cb9dba03cf66d7209ca14108
Alex <oleksandr.khmil2@gmail.com>
2020-03-29 11:36:46 +0300

    1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.get('/q', (req, res) => res.send('q1wq'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
