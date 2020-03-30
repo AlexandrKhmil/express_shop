a4533dd9369b688d91dd02cf3c766f7fb6b68820
Alex <oleksandr.khmil2@gmail.com>
2020-03-30 16:54:14 +0300

    added admin panel

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const mysqlAdmin = require('node-mysql-admin');
+app.use(mysqlAdmin(app));
 
 const connection = require('./connection')
 
